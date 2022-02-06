3df06e19dc8e76b1d57bf06b6171eabd4932cafb
Fabio Plombon <fplombon@alltech-brazil.com>
2022-02-06 17:43:18 -0300

    Projeto até o ponto da live

--- script.js
+++ script.js
@@ -45,6 +45,18 @@ function startGame(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+            if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            let end = document.getElementById('somGameover')    
+            clearInterval(game);
+            end.play()
+            //alert('Game Over')
+            document.getElementById('scoreboard').innerHTML = '<h1>FIM DE JOGO</h1>'
+            
+
+        }
+    }
+
     createBackground();
     createSnake();
     drawFood();
